352ca09774f0c012ad463dbec797138da2bf37ab
sujizhen <479428129@qq.com>
2022-03-18 10:49:35 +0800

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup /home/coder/code-server/code-server --port 1080 --host 0.0.0.0 --auth password >/dev/null 2>&1 &
+nohup /root/coder/code-server/code-server --port 1080 --host 0.0.0.0 --auth password >/dev/null 2>&1 &
 nginx -g "daemon off;"
